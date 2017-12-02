3ee1d95f2ed5b99423a7002603d5e9603ae93311
Deepak Bhetuwal <dkb208@msstate.edu>
2017-12-02 01:34:22 -0500

    fixed typos and changed files for PRODUCTION

--- SCRIPTS/COIN/PRODUCTION/replay_production_coin.C
+++ SCRIPTS/COIN/PRODUCTION/replay_production_coin.C
@@ -24,7 +24,7 @@ void replay_production_coin (Int_t RunNumber = 0, Int_t MaxEvent = 0) {
     pathList.push_back("./cache");
 
   //const char* RunFileNamePattern = "raw/coin_all_%05d.dat";
-  const char* ROOTFileNamePattern = "ROOTfiles/coin_replay_production_%d_%d.root";
+  const char* ROOTFileNamePattern = "ROOTfiles/shms_replay_production_%d_%d.root";
   
   // Load global parameters
   // Add variables to global list.
